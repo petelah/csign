d70d09a2af5777f27d54c5a0f4ae86222d08edf5
Peter Seabrook <seabrook.peter@gmail.com>
2020-11-07 17:31:45 +1100

    submit signin complete

--- src/main/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-class TodoForm(FlaskForm):
-    content = TextAreaField('Add your todo:', validators=[DataRequired()])
-    submit = SubmitField('Add')
--- src/users/routes.py
+++ src/users/routes.py
@@ -47,9 +47,9 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+            return redirect(next_page) if next_page else redirect(url_for('users.account'))
         else:
-            flash('Login not work. Please check email and password', 'danger')
+            flash('Login not found. Please check email and password', 'danger')
     return render_template('login.html', title='login', form=form)
 
 
